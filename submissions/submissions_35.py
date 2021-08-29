6baa2d1e02c2b0b27f7a477cd21b34401d1f8bcc
Arnab Biswas <arnabbiswas1@outlook.com>
2021-08-29 12:11:03 +0530

    Blending based submissions

--- submissions/submissions_35.py
+++ submissions/submissions_35.py
@@ -5,8 +5,8 @@ import pandas as pd
 COMPETITION_NAME = "tabular-playground-series-aug-2021"
 
 SUBMISSION_DIR = "."
-SUBMISSION_FILE = "sub_blending_public_7_84995_lgb_ts_f1_score_top_50_0828_1259.csv"
-SUBMISSION_MESSAGE = '"LGB SK(10),freq enc,params from K,seed 20 & public sub with 7.84995"'
+SUBMISSION_FILE = "sub_blending_public_7_84995_lgb_freq_enc_params_from_Kaggle_seed_20_and_poisson_0828_2146.csv"
+SUBMISSION_MESSAGE = '"LGB, SK(10), freq enc, params from K, seed 20 & poisson & public sub with 7.84995 (0.09/0.01)"'
 
 df = pd.read_csv(f"{SUBMISSION_DIR}/{SUBMISSION_FILE}")
 print(df.head())
